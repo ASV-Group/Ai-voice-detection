ad7c3f881ebc669aa06e358b629031742271991b
AmAn SonwAni <96755747+CyberMatic-AmAn@users.noreply.github.com>
2026-09-08 16:37:53 +0530

    choose-model-to-download-settings

--- download_model.py
+++ download_model.py
@@ -1,8 +1,10 @@
 from huggingface_hub import snapshot_download
 
-MODEL_ID = "facebook/wav2vec2-large-xlsr-53"
+# MODEL_ID = "facebook/wav2vec2-large-xlsr-53"
+MODEL_ID = "garystafford/wav2vec2-deepfake-voice-detector"
 
-MODEL_PATH = "./models/wav2vec2-large-xlsr-53"
+# MODEL_PATH = "./models/wav2vec2-large-xlsr-53"
+MODEL_PATH = "./models/wav2vec2-deepfake-voice-detector"
 
 print("Downloading model...")
 print(f"Model: {MODEL_ID}")
