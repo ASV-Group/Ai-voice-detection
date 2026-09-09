07ed1f2f063beccced4f382220131bbf03cc71c1
Vaibhav <coder8809@gmail.com>
2026-09-09 23:22:57 +0530

    changing server.py

--- backend/server.py
+++ backend/server.py
@@ -98,4 +98,4 @@ async def check_real_audio(websocket: WebSocket):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host = "0.0.0.0", port = 8000, reload = True)
+    uvicorn.run("server:app", host = "0.0.0.0", port = 8000, reload = True)
